676df44a20385a5dbf69f95b2c0ad3b49812fd06
highsky134 <sangchun134@gmail.com>
2023-05-08 19:06:00 +0900

    # first upload

---
+++ src/main/java/com/trifling/triflings/TriflingsApplication.java
@@ -0,0 +1,13 @@
+package com.trifling.triflings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriflingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriflingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trifling/triflings/TriflingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trifling.triflings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriflingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
